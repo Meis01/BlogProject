60b2121e865819669b710f46ef13b9cb51336c80
Meis01 <meis.baas95@hotmail.com>
2021-05-09 14:05:12 +0300

    Core, service katmanları tamamlandı. Model katmanlı geliştirimeye devam ediliyor.

---
+++ BlogProject/BlogProject.MODEL/Maps/CategoryMap.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BlogProject.MODEL.Maps
+{
+    class CategoryMap
+    {
+    }
+}
---
+++ BlogProject/BlogProject.MODEL/Maps/UserMap.cs
@@ -0,0 +1,21 @@
+﻿using BlogProject.CORE.Entity.Map;
+using BlogProject.MODEL.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BlogProject.MODEL.Maps
+{
+    public class UserMap : CoreMap<User>
+    {
+        public override void Configure(EntityTypeBuilder<User> builder)
+        {
+            builder.ToTable("Users");
+            // Burada Kalacak
+            base.Configure(builder);
+        }
+    }
+}
