845235b2974ee973b93fded950088c43b582f299
Meis01 <meis.baas95@hotmail.com>
2021-05-16 11:36:30 +0300

    Context'e güncelledik

--- BlogProject/BlogProject.MODEL/Context/BlogContext.cs
+++ BlogProject/BlogProject.MODEL/Context/BlogContext.cs
@@ -1,12 +1,67 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using BlogProject.CORE.Concrete;
+using BlogProject.MODEL.Entities;
+using BlogProject.MODEL.Maps;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace BlogProject.MODEL.Context
 {
    public class BlogContext : DbContext
     {
+        public BlogContext(DbContextOptions<BlogContext> options) : base(options)
+        {
 
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new CategoryMap());
+            modelBuilder.ApplyConfiguration(new CommentMap());
+            modelBuilder.ApplyConfiguration(new PostMap());
+            modelBuilder.ApplyConfiguration(new UserMap());
+
+            base.OnModelCreating(modelBuilder);
+        }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+        public DbSet<Post> Posts { get; set; }
+        public DbSet<User> Users { get; set; }
+
+        public override int SaveChanges()
+        {
+            var gelenEntities = ChangeTracker.Entries()
+                .Where(x => x.State == EntityState.Modified || x.State == EntityState.Added).ToList();
+
+            string computerName = Environment.MachineName;
+            string ipAddress = "127.0.0.1";
+            DateTime date = DateTime.Now;
+
+            foreach (var item in gelenEntities)
+            {
+                CoreEntity entity = item.Entity as CoreEntity;
+                if (item != null)
+                {
+                    switch (item.State)
+                    {
+                        case EntityState.Added:
+                            entity.CreatedComputerName = computerName;
+                            entity.CreatedIP = ipAddress;
+                            entity.CreatedDate = date;
+                            break;
+                        case EntityState.Modified:
+                            entity.ModifiedComputerName = computerName;
+                            entity.CreatedIP = ipAddress;
+                            entity.CreatedDate = date;
+                            break;
+                      
+                       
+                    }
+                }
+            }
+
+            return base.SaveChanges();
+        }
     }
 }
